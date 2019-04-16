d262c7cb5aa859e56248f454da62316d74599216
QDSG <piftao1127@163.com>
2019-04-16 14:00:31 +0800

    添加一个 Stateful widget（有状态的widget）

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -6,8 +6,6 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -15,9 +13,23 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: new Center(
-          child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(),
         ),
       ),
     );
+  }
+}
+
+// 有状态的 widget
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => new _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
