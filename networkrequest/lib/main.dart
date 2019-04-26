e43778abae781804acae54c6daa91b24209d1947
QDSG <piftao1127@163.com>
2019-04-26 10:26:25 +0800

    【Flutter】怎么把工作放到后台线程的？

--- networkrequest/lib/main.dart
+++ networkrequest/lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'dart:isolate';
+import 'dart:async';
 
 void main() => runApp(SampleApp());
 
@@ -40,13 +42,36 @@ class _SampleAppPageState extends State<SampleAppPage> {
       appBar: AppBar(
         title: Text('Sample App'),
       ),
-      body: ListView.builder(
-        itemCount: widgets.length,
-        itemBuilder: (BuildContext context, int position) {
-          return _getRow(position);
-        },
-      ),
+      body: _getBody(),
     );
+  }
+
+  bool _showLoadingDialog() {
+    if (widgets.length == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Widget _getProgressDialog() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  ListView _getListView() => ListView.builder(
+      itemCount: widgets.length,
+      itemBuilder: (BuildContext context, int position) {
+        return _getRow(position);
+      });
+
+  Widget _getBody() {
+    if (_showLoadingDialog()) {
+      return _getProgressDialog();
+    } else {
+      return _getListView();
+    }
   }
 
   Widget _getRow(int index) {
@@ -56,6 +81,52 @@ class _SampleAppPageState extends State<SampleAppPage> {
     );
   }
 
+  loadData() async {
+    ReceivePort receivePort = ReceivePort();
+    await Isolate.spawn(dataLoader, receivePort.sendPort);
+
+    SendPort sendPort = await receivePort.first;
+
+    List msg = await sendReceive(
+        sendPort, 'https://jsonplaceholder.typicode.com/posts');
+
+    setState(() {
+      widgets = msg;
+    });
+  }
+
+  static dataLoader(SendPort sendPort) async {
+    ReceivePort port = ReceivePort();
+    sendPort.send(port.sendPort);
+    await for (var msg in port) {
+      String data = msg[0];
+      SendPort replyTo = msg[1];
+
+      String dataURL = data;
+      HttpClient httpClient = HttpClient();
+      try {
+        HttpClientRequest request = await httpClient.getUrl(Uri.parse(dataURL));
+        HttpClientResponse response = await request.close();
+        if (response.statusCode == HttpStatus.ok) {
+          String json = await response.transform(utf8.decoder).join();
+          replyTo.send(jsonDecode(json));
+        } else {
+          print(
+              'Error getting IP address:\nHttp status ${response.statusCode}');
+        }
+      } catch (exception) {
+        print('Failed getting IP address');
+      }
+    }
+  }
+
+  Future sendReceive(SendPort port, msg) {
+    ReceivePort response = ReceivePort();
+    port.send([msg, response.sendPort]);
+    return response.first;
+  }
+
+/**
   loadData() async {
     String dataURL = 'https://jsonplaceholder.typicode.com/posts';
     HttpClient httpClient = HttpClient();
@@ -74,4 +145,5 @@ class _SampleAppPageState extends State<SampleAppPage> {
       print('Failed getting IP address');
     }
   }
+  */
 }
